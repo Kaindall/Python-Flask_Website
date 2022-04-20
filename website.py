e60f974e3107afb425853ce3de66aa7593c22f25
Kaindall <gustavozanete2@gmail.com>
2022-04-20 00:08:23 -0300

    Second edit
    OK, I'm creating other files to every page on the website to I use just the principal file to execute the code.

--- website.py
+++ website.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'OK, my site is working'
+    return render_template('homepage.html')
 
-
-
+@app.route('/sobre_nos')
+def us():
+    return render_template('sobre_nos')
 
 if __name__ == '__main__':
     app.run(debug=True)
