d7b91050c9bc433a1073b024d857d79f41b71472
Kaindall <gustavozanete2@gmail.com>
2022-04-22 17:12:34 -0300

    update 2
    starting to create user's page and a central page with default layout

--- website.py
+++ website.py
@@ -5,9 +5,13 @@ app=Flask(__name__)
 def home():
     return render_template('homepage.html')
 
-@app.route('/sobre_nos')
-def us():
-    return render_template('sobre_nos')
+@app.route('/contact')
+def contact():
+    return render_template('contact')
+
+@app.route('/users')
+def users
+    return render_template('users')
 
 if __name__ == '__main__':
     app.run(debug=True)
