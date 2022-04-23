f91189f4855254d90347522ff56d33b9d47e9308
Kaindall <gustavozanete2@gmail.com>
2022-04-23 01:15:09 -0300

    fourth update
    I'm started to create a navigation bar, but to edit it is so deep that I need to get time tomorrow to do it with time and my mind clearly. I'm also created a list of users printed on the page.

--- website.py
+++ website.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template
 app=Flask(__name__)
 
+user_list = ['Jax', 'Fiora', 'Gnar', 'Khazix', 'Viktor']
+
 @app.route('/')
 def home():
     return render_template('homepage.html')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact')
+    return render_template('contact.html')
 
 @app.route('/users')
 def users():
-    return render_template('users')
+    return render_template('users.html', user_list=user_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
