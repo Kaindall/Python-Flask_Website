fda41c734f16e04363dcbcbe8ef3d733b972495f
Kaindall <gustavozanete2@gmail.com>
2022-05-05 21:22:21 -0300

    update (05/05/2022)
    finish of the forms changing the appearance to be beautiful and redirect link back to the homepage with a message... I also insert a red flag if any label is empty

--- forms.py
+++ forms.py
@@ -5,13 +5,13 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class FormCreateAcc(FlaskForm):
     username = StringField('Nickname', validators=[DataRequired()])
-    email = StringField('E-mail adress', validators=[DataRequired(), Email()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     pw_confirmation = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit_createaccount = SubmitField('Create Account')
+    btnsubmit_createaccount = SubmitField('Create Account')
 
 class FormLogin(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     remember_login = BooleanField('Remember me')
-    submit_login = SubmitField('Login')
+    btnsubmit_login = SubmitField('Login')
--- website.py
+++ website.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormCreateAcc, FormLogin
+
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = 'I15NxqBiJ7R9nnUWhM3Nd5dCu3TUM8su'
@@ -21,10 +22,17 @@ def contact():
 def users():
     return render_template('users.html', user_list=user_list)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     f_login = FormLogin()
     f_createacc = FormCreateAcc()
+    print('Ok, here')
+    if f_login.validate_on_submit() and 'btnsubmit_login' in request.form:
+        flash(f'Login successfully! Welcome {f_login.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+    if f_createacc.validate_on_submit() and 'btnsubmit_createaccount' in request.form:
+        flash(f'Account created, nice to meet you {f_createacc.username.data}.', 'alert-success')
+        return redirect(url_for('home'))
     return render_template('login.html', f_login=f_login, f_createacc=f_createacc)
 
 
